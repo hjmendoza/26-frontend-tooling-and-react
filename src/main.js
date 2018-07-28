01a7b8a446c701fc5282a491d9dfdf97ed6f65d1
Haley Mendoza <mendozahaley@gmail.com>
2018-07-28 12:32:46 -0700

    ccomplted lab

--- src/main.js
+++ src/main.js
@@ -1,1 +1,43 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { say } from 'cowsay';
 
+
+const cowsay = require('cowsay');
+const faker = require('faker/locale/de');
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = { content: 'I say things' }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(){
+   this.updateState(this.state.content);
+  }
+
+  updateState(content) {
+    content = faker.lorem.words(10);
+
+  this.setState({
+    content,
+  });
+}
+
+  render(){
+    return (
+    <React.Fragment>
+      <h1>Generate Cowsay Lorem</h1>
+      <button onClick={this.handleClick}>click me</button>
+      <pre>
+        {cowsay.say({
+          text: this.state.content,
+        })}
+      </pre>
+    </React.Fragment>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
